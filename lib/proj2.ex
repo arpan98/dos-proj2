ce41106076d8683b85dec58c8247db8058f66a24
Arpan Banerjee <arpanbnrj9@gmail.com>
2019-09-28 14:45:32 -0400

    added line, honeycombs topologies

--- lib/proj2.ex
+++ lib/proj2.ex
@@ -14,22 +14,35 @@ defmodule Proj2 do
   """
 
   def main(args) do
-    try do
+    # try do
       [numNodes, topology, algorithm] = args |> parse_input
+      numNodes = round_up(numNodes, topology)
       {:ok, statsPID} = GenServer.start_link(Stats, [numNodes])
       Topology.create_topology(numNodes, topology, statsPID)
       {_, pid, _, _} = Supervisor.which_children(GossipSupervisor) |> Enum.random
       GenServer.call(statsPID, :startTimer)
       GenServer.cast(pid, {:gossip, "psst"})
       loop()
-    rescue
-      FunctionClauseError -> IO.puts("3 arguments expected - numNodes(int) topology algorithm")
-      ArgumentError -> IO.puts("3 arguments expected - numNodes(int) topology algorithm")
-    end
+    # rescue
+    #   FunctionClauseError -> IO.puts("3 arguments expected - numNodes(int) topology algorithm")
+    #   ArgumentError -> IO.puts("3 arguments expected - numNodes(int) topology algorithm")
+    # end
   end
 
   def parse_input([numNodes, topology, algorithm]) do
     [String.to_integer(numNodes), topology, algorithm]
+  end
+
+  defp round_up(numNodes, topology) do
+    case topology do
+      t when t in ["full", "line", "rand2D"] -> numNodes
+
+      "3Dtorus" ->
+        numNodes |> :math.sqrt() |> ceil() |> :math.pow(3) |> trunc()
+      
+      t when t in ["honeycomb", "randhoneycomb"] -> 
+        numNodes |> :math.sqrt() |> ceil() |> :math.pow(2) |> trunc()
+    end
   end
 
   def loop() do
--- lib/topology.ex
+++ lib/topology.ex
@@ -1,8 +1,8 @@
 defmodule Topology do
+  require Integer
 
   def create_topology(numNodes, topology, statsPID) do
     IO.puts("Topology #{topology} with #{numNodes} nodes")
-
     nodes = 1..numNodes
 
     children = nodes
@@ -15,25 +15,107 @@ defmodule Topology do
       {_, pid, _, _} = child
       pid
     end)
+
+    nodes = Enum.zip(nodes, 1..numNodes)
+
     Enum.each(nodes, fn node ->
-      assign_neighbors(node, nodes) |> send_neighbors()
+      get_neighbors(nodes, node, topology)
+      |> get_pids_from_indices(nodes)
+      |> assign_neighbors(node)
+      |> send_neighbors()
     end)
   end
 
   defp send_neighbors({cur_node, neighbors}) do
+    {neighbors, _} = Enum.unzip(neighbors)
+    IO.inspect(neighbors)
     GenServer.cast(cur_node, {:neighbors, neighbors})
   end
 
-  def assign_neighbors(cur_node, nodes) do
-    {cur_node, Enum.reject(nodes, fn x -> x == cur_node end)}
+  def assign_neighbors(nodes, cur_node) do
+    {cur_pid, _} = cur_node
+    {cur_pid, nodes}
   end
 
-  defp get_neighbors(nodes, nodeIndex, topology) do
-    case topology do
-      "full" -> nodes
-        |> Enum.reject(fn x -> x == nodeIndex end)
-      _ -> IO.puts("Currently only supporting full topology")
+  defp get_neighbors(nodes, cur_node, topology) when topology == "full" do
+    {_, nodeIndex} = cur_node
+    {_, nodeIndices} = Enum.unzip(nodes)
+    nodeIndices |> Enum.reject(fn x -> x == nodeIndex end)
+  end
+
+  defp get_neighbors(nodes, cur_node, topology) when topology == "line" do
+    {_, nodeIndex} = cur_node
+    numNodes = Enum.count(nodes)
+    cond do
+      nodeIndex == 1 -> [nodeIndex + 1]
+      nodeIndex == numNodes -> [nodeIndex - 1]
+      true -> [nodeIndex - 1, nodeIndex + 1]
     end
+  end
+
+  defp get_neighbors(nodes, cur_node, topology) when topology == "honeycomb" do
+    {_, nodeIndex} = cur_node
+    numNodes = Enum.count(nodes)
+    get_honeycomb_neighbors(numNodes, nodeIndex)
+  end
+
+  defp get_neighbors(nodes, cur_node, topology) when topology == "randhoneycomb" do
+    {_, nodeIndex} = cur_node
+    {_, nodeIndices} = Enum.unzip(nodes)
+    numNodes = Enum.count(nodes)
+    neighbors = get_honeycomb_neighbors(numNodes, nodeIndex)
+    rand_neighbor = nodeIndices
+    |> Enum.reject(fn x -> x == nodeIndex end)  # Remove self node
+    |> Enum.reject(fn x -> Enum.member?(neighbors, x) end)  # Remove already neighbor nodes
+    |> Enum.random()
+    [rand_neighbor | neighbors]
+  end
+
+  defp get_honeycomb_neighbors(numNodes, nodeIndex) do
+    n = :math.sqrt(numNodes) |> trunc()
+    cond do
+      # Corner nodes
+      nodeIndex == 1 -> [nodeIndex + n]
+      nodeIndex == numNodes -> [nodeIndex - 1, nodeIndex - n]
+
+      nodeIndex == n ->
+        cond do
+          Integer.is_odd(n) -> [nodeIndex - 1, nodeIndex + n]
+          Integer.is_even(n) -> [nodeIndex + n]
+        end
+
+      nodeIndex == numNodes - n + 1 ->
+        cond do
+          Integer.is_odd(n) -> [nodeIndex - n]
+          Integer.is_even(n) -> [nodeIndex-n, nodeIndex + 1]
+        end
+
+      # First row
+      nodeIndex < n ->
+        cond do
+          Integer.is_odd(nodeIndex) -> [nodeIndex - 1, nodeIndex + n]
+          Integer.is_even(nodeIndex) -> [nodeIndex + 1, nodeIndex + n]  
+        end
+
+      # Last row
+      nodeIndex > numNodes - n ->
+        cond do
+          Integer.is_odd(nodeIndex) -> [nodeIndex - 1, nodeIndex - n]
+          Integer.is_even(nodeIndex) -> [nodeIndex + 1, nodeIndex - n]  
+        end
+
+      # First and last column
+      rem(nodeIndex, 2*n) in [1, n] -> [nodeIndex - n, nodeIndex + n]
+
+      # All internal nodes
+      Integer.is_odd(nodeIndex) -> [nodeIndex + 1, nodeIndex - n, nodeIndex + n]
+
+      Integer.is_even(nodeIndex) -> [nodeIndex - 1, nodeIndex - n, nodeIndex + n]
+    end
+  end
+
+  defp get_pids_from_indices(neighbors, nodes) do
+    Enum.filter(nodes, fn ({_, index}) -> Enum.member?(neighbors, index) end)
   end
 
 end
